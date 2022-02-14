690795eb83aa33c20ff4a03d9463fe3b96819a3a
Patrick Hettich <patrick_hettich@hotmail.com>
2022-02-14 17:24:34 +0100

    animated tileset test

--- flutter_simple_plattformer/assets/tiles/Spritesheet.tsx
+++ flutter_simple_plattformer/assets/tiles/Spritesheet.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Spritesheet" tilewidth="32" tileheight="32" tilecount="7" columns="7">
+<tileset version="1.8" tiledversion="1.8.1" name="Spritesheet" tilewidth="32" tileheight="32" tilecount="7" columns="7">
  <image source="../images/Spritesheet.png" width="224" height="32"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
 </tileset>
