248c4c7f67cc4bfbb66ab9345d99325d8e54cadc
Patrick Hettich <patrick_hettich@hotmail.com>
2022-02-17 20:21:27 +0100

    testing sprites updated

--- flutter_simple_plattformer/assets/tiles/Terrain.tsx
+++ flutter_simple_plattformer/assets/tiles/Terrain.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="Terrain" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="../images/sprites.png" width="320" height="320"/>
+<tileset version="1.8" tiledversion="1.8.1" name="Terrain" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="../images/sprites.png" width="256" height="64"/>
 </tileset>
